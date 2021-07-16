2c50ded7b5008f264859ee3c069e9e9f2f0bb889
xuetengfei <workingxue@gmail.com>
2021-07-16 20:57:30 +0800

    ffacc38812d7e07cf61d2b69e9fb9c8a

--- TinyDB_operation.py
+++ TinyDB_operation.py
@@ -39,7 +39,7 @@ goal = {
 
 
 # db.table('all').update(
-#     {'catalog': 'node', 'describtion': '如何正确的学习Node.js4'}, doc_ids=[4])
+#     {'describtion': '如何正确的学习Node.js', 'describe': '如何正确的学习Node.js'}, doc_ids=[4])
 
 
 # DeleteItem(270)
