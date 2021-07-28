4dba1bc9682ecd38fa10f10ab53397c9ab938fe8
xuetengfei <workingxue@gmail.com>
2021-07-28 23:05:42 +0800

    fda6627daddb83243d35915e111f870d

--- TinyDB_operation.py
+++ TinyDB_operation.py
@@ -22,11 +22,13 @@ def DeleteItem(id):
     db.table('all').remove(doc_ids=[id])
     print('Delete Item Successed !!!')
 
+# [冲浪普拉斯的个人空间_哔哩哔哩_Bilibili]()
+
 
 goal = {
-    'describtion': 'TypeScript 文档教程',
-    'url': 'https://www.javascriptcn.com/typescript/ts-tutorial.html',
-    "catalog": 'node'  # 小写
+    'describtion': '冲浪普拉斯-商业侃哥',
+    'url': 'https://space.bilibili.com/290548469/video',
+    "catalog": '因吹斯汀'  # 小写
 }
 
 # AddItem(goal)
