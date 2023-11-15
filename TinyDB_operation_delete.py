14518ab8d7aae9b763396de938de0544b8cc035c
xuetengfei <workingxue@gmail.com>
2023-11-16 01:24:00 +0800

    747f8987ce2b5d4e6ce91f4ad81e9170

---
+++ TinyDB_operation_delete.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+# coding:utf-8
+
+from tinydb import TinyDB
+import os
+
+DB_FILE = "db.json"
+TABLE_NAME = "all"
+
+
+def get_database_path():
+    script_directory = os.path.dirname(os.path.abspath(__file__))
+    return os.path.join(script_directory, DB_FILE)
+
+
+def delete_items(db, table_name, item_ids):
+    table = db.table(table_name)
+    table.remove(doc_ids=item_ids)
+    print("Delete Item(s) Successful!")
+
+
+def main():
+    db_path = get_database_path()
+    db = TinyDB(db_path)
+
+    # Example: Delete items with specified IDs
+    item_ids_to_delete = [41]
+    delete_items(db, TABLE_NAME, item_ids_to_delete)
+
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
